2334e177d94610fe11db468857882879154b1d04
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-04-19 13:51:44 +0200

    applied fractal binary tree rules to axiom provided by user in command line arguments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <raylib.h>
-
-int main()
+#include <string>
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
 {
-    int sw = 960, sh = 720;
+    if (argc != 2)
+        return 1;
+    string curr = argv[1]; // axiom
+    float sw = 960.0f, sh = 720.0f;
     InitWindow(sw, sh, "L-system");
     while (!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_ENTER))
+        {
+            string buffer = "";
+            for (char ch : curr)
+            {
+                if (ch == '1')
+                    buffer += "11";
+                else if (ch == '0')
+                    buffer += "1[0]0";
+                else
+                    buffer += ch;
+            }
+            curr = buffer;
+            cout << "current: " << curr << endl;
+        }
         BeginDrawing();
         ClearBackground(BLACK);
         EndDrawing();
