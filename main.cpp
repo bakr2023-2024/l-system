6929b4bc88ef29292346a7c63e7cde670a8ece39
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-04-19 14:10:52 +0200

    rendered fractal binary tree l-system

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <raylib.h>
 #include <string>
-#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <stack>
+struct State
+{
+    Vector2 pos;
+    float angle;
+};
 using namespace std;
 int main(int argc, char **argv)
 {
@@ -8,6 +15,12 @@ int main(int argc, char **argv)
         return 1;
     string curr = argv[1]; // axiom
     float sw = 960.0f, sh = 720.0f;
+    /*
+        we apply rotation matrix to len vector <0,L> to get <-L*sin(theta),L*cos(theta)>
+        then add the vector to start position to get end position <x - L*sin(theta), y - L*cos(theta)>
+        # we subtract L*cos(theta) from y since in pixel coordinates, y-axis moves down unlike 2d coordinate system
+    */
+    float turn = M_PI / 4;
     InitWindow(sw, sh, "L-system");
     while (!WindowShouldClose())
     {
@@ -24,10 +37,41 @@ int main(int argc, char **argv)
                     buffer += ch;
             }
             curr = buffer;
-            cout << "current: " << curr << endl;
         }
+        Vector2 pos{sw / 2, sh};
+        float angle = 0.0f;
+        float L = 1;
+        stack<State> s;
         BeginDrawing();
         ClearBackground(BLACK);
+        for (char ch : curr)
+        {
+            if (ch == '0')
+            {
+                Vector2 newPos{pos.x - L * sinf(angle), pos.y - L * cosf(angle)};
+                DrawLineV(pos, newPos, WHITE);
+                DrawEllipse(newPos.x, newPos.y, 2, 2, GREEN);
+                pos = newPos;
+            }
+            else if (ch == '1')
+            {
+                Vector2 newPos{pos.x - L * sinf(angle), pos.y - L * cosf(angle)};
+                DrawLineV(pos, newPos, WHITE);
+                pos = newPos;
+            }
+            else if (ch == '[')
+            {
+                s.push({pos, angle});
+                angle -= turn;
+            }
+            else if (ch == ']')
+            {
+                State state = s.top();
+                pos = state.pos;
+                angle = state.angle + turn;
+                s.pop();
+            }
+        }
         EndDrawing();
     }
     CloseWindow();
