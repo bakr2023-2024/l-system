0f29183a8e6433fe8914d1cfb39aed7b125479cb
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-04-25 16:13:33 +0300

    added input validation for file argument and config parameters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <raylib.h>
 #include "utils.hpp"
-using namespace std;
+#include <iostream>
 struct State
 {
     Vector2 pos;
@@ -8,33 +8,40 @@ struct State
 };
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc != 2 || !std::string(argv[1]).ends_with(".txt"))
+    {
+        std::cout << "Please provide a file name(.txt) as an argument\n";
         return 1;
-    string curr = argv[1]; // axiom
-    float sw = 960.0f, sh = 720.0f;
+    }
+    Parameters params{};
+    if (!parse(argv[1], params))
+    {
+        std::cout << "File '" << argv[1] << "' doesn't exist";
+        return 1;
+    };
+    if (params.axiom.empty())
+    {
+        std::cout << "An axiom is required to start the L-system\n";
+        return 1;
+    }
     /*
         we apply rotation matrix to len vector <0,L> to get <-L*sin(theta),L*cos(theta)>
         then add the vector to start position to get end position <x - L*sin(theta), y - L*cos(theta)>
         note: we subtract L*cos(theta) from y since in pixel coordinates, y-axis moves down
     */
     float L = 20;
-    float turn = 2 * M_PI / 3.0;
+    float turn = params.angle * M_PI / 180;
+    std::string curr = params.axiom;
+    float sw = 960.0f, sh = 720.0f;
     InitWindow(sw, sh, "L-system");
     while (!WindowShouldClose())
     {
         if (IsKeyPressed(KEY_ENTER))
         {
             L *= 0.9f;
-            string buffer = "";
+            std::string buffer = "";
             for (char ch : curr)
-            {
-                if (ch == 'F')
-                    buffer += "F-G+F+G-F";
-                else if (ch == 'G')
-                    buffer += "GG";
-                else
-                    buffer += ch;
-            }
+                buffer += params.rules.contains(ch) ? params.rules.at(ch) : std::string{ch};
             curr = buffer;
         }
         Vector2 pos{10, sh};
